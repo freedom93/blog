d6b0399ca95f6880f207dc347c4bff44164b9e6b
lifen <lifen@yy.com>
2017-08-29 17:07:24 +0800

    引入bootstrap bootstrap.js依赖jquery、popper.js所以要在webpack中配置这两个插件

--- dev/src/main.js
+++ dev/src/main.js
@@ -3,6 +3,8 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import 'bootstrap/js/bootstrap.js'
+import 'bootstrap/css/bootstrap.css'
 
 Vue.config.productionTip = false
 
